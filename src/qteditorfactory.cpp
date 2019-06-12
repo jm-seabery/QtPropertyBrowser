643d55bb36ce2e4cb29ca9f5fe4fe35f550130ee
jm-seabery <jm@seaberyat.com>
2019-06-12 13:12:48 +0200

    [John Murray] Fix the colour selector

--- src/qteditorfactory.cpp
+++ src/qteditorfactory.cpp
@@ -2205,11 +2205,14 @@ void QtColorEditWidget::setValue(const QColor &c)
 
 void QtColorEditWidget::buttonClicked()
 {
-    bool ok = false;
-    QRgb oldRgba = m_color.rgba();
-    QRgb newRgba = QColorDialog::getRgba(oldRgba, &ok, this);
-    if (ok && newRgba != oldRgba) {
-        setValue(QColor::fromRgba(newRgba));
+    //bool ok = false;
+    //QRgb oldRgba = m_color.rgba();
+    //QRgb newRgba = QColorDialog::getRgba(oldRgba, &ok, this);
+    
+    QColor oldRgba = m_color;
+    QColor newRgba = QColorDialog::getColor(m_color, this, "Select Color", QColorDialog::DontUseNativeDialog | QColorDialog::ShowAlphaChannel );
+    if (/*ok &&*/ newRgba != oldRgba) {
+        setValue(newRgba);
         emit valueChanged(m_color);
     }
 }
